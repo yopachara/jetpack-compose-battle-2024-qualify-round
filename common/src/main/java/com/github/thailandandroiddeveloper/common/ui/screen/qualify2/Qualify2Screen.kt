cf0543589891cf4fbe7c452cf6ca775b9f4f9396
yopachara <yopachara@gmail.com>
2024-01-18 01:59:14 +0700

    feat: Update UI Qualify2

--- common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify2/Qualify2Screen.kt
+++ common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify2/Qualify2Screen.kt
@@ -1,13 +1,162 @@
 package com.github.thailandandroiddeveloper.common.ui.screen.qualify2
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.thailandandroiddeveloper.common.R
 import com.github.thailandandroiddeveloper.common.ui.preview.Pixel7
 import com.github.thailandandroiddeveloper.common.ui.theme.AppTheme
 
+
 @Composable
 fun Qualify2Screen() {
-    // TODO: Qualify2
+    Scaffold(
+        topBar = {
+            TopContent(onSkipClicked = {
+                // TODO: Need to implement
+            })
+        },
+        bottomBar = {
+            BottomContent(onNextClicked = {
+                // TODO: Need to implement
+            })
+        }
+    ) { inner ->
+        Qualify2Content(inner)
+
+    }
+}
+
+@Composable
+fun Qualify2Content(inner: PaddingValues) {
+    Box(modifier = Modifier.padding(inner)) {
+        TutorialContent()
+    }
+}
+
+@Composable
+fun TutorialContent() {
+    Column(modifier = Modifier.padding(horizontal = 32.dp)) {
+        Text(
+            text = "Lorem ipsum dolor sit amet",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.headlineSmall,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas dictum lacinia. Integer arcu neque, porttitor ac metus quis, iaculis molestie magna. Vivamus molestie justo sed nulla lacinia, quis fringilla lorem imperdiet. Proin in quam vel odio iaculis fringilla",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.img_qualify_2_onboard),
+            contentDescription = "image description",
+            contentScale = ContentScale.FillBounds
+        )
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(1f)
+        ) {
+            DotIndicator(totalPage = 5, currentPage = 3)
+        }
+    }
+}
+
+@Composable
+private fun TopContent(onSkipClicked: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(horizontalArrangement = Arrangement.End, modifier = Modifier.fillMaxWidth()) {
+            TextButton(onClick = onSkipClicked) {
+                Text(
+                    text = "Skip",
+                    style = MaterialTheme.typography.labelLarge,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun BottomContent(onNextClicked: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.primaryContainer)
+            .fillMaxWidth()
+            .height(102.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 80.dp),
+            onClick = onNextClicked
+        ) {
+            Text(text = "Next", style = MaterialTheme.typography.labelLarge)
+        }
+    }
+}
+
+@Composable
+private fun DotIndicator(totalPage: Int, currentPage: Int) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        repeat(totalPage) { index ->
+            val isSelected = index == currentPage
+            Box(
+                modifier = Modifier
+                    .size(width = if (isSelected) 32.dp else 16.dp, height = 16.dp)
+                    .background(
+                        if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.primaryContainer,
+                        shape = CircleShape
+                    )
+            )
+        }
+    }
 }
 
 // region Read-only because we use this to process your score.

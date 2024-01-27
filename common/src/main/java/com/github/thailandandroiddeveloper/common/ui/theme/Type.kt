fe3263199a916d621fc0182babe80a2e63bf811c
yopachara <yopachara@gmail.com>
2024-01-27 22:42:24 +0700

    update type not matching

--- common/src/main/java/com/github/thailandandroiddeveloper/common/ui/theme/Type.kt
+++ common/src/main/java/com/github/thailandandroiddeveloper/common/ui/theme/Type.kt
@@ -15,7 +15,6 @@ val Typography = Typography(
         fontWeight = FontWeight.W400,
         fontSize = 57.sp,
         lineHeight = 64.sp,
-        letterSpacing = (-0.25).sp
     ),
     displayMedium = TextStyle(
         fontFamily = FontFamily.Default,
@@ -45,14 +44,14 @@ val Typography = Typography(
         fontWeight = FontWeight.W400,
         fontSize = 28.sp,
         lineHeight = 36.sp,
-        letterSpacing = .0.sp
+        letterSpacing = .4.sp
     ),
     headlineSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.W400,
         fontSize = 24.sp,
         lineHeight = 32.sp,
-        letterSpacing = .0.sp
+        letterSpacing = .5.sp
     ),
 
     // Title
@@ -61,14 +60,14 @@ val Typography = Typography(
         fontWeight = FontWeight.W400,
         fontSize = 22.sp,
         lineHeight = 28.sp,
-        letterSpacing = .0.sp
+        letterSpacing = 0.5.sp
     ),
     titleMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.W500,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.15.sp
+        letterSpacing = 0.50.sp
     ),
     titleSmall = TextStyle(
         fontFamily = FontFamily.Default,
@@ -84,7 +83,7 @@ val Typography = Typography(
         fontWeight = FontWeight.W500,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.1.sp
+        letterSpacing = 0.5.sp
     ),
     labelMedium = TextStyle(
         fontFamily = FontFamily.Default,
@@ -114,13 +113,12 @@ val Typography = Typography(
         fontWeight = FontWeight.W400,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.25.sp
+        letterSpacing = 0.5.sp
     ),
     bodySmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.W400,
         fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.4.sp
     ),
 )
